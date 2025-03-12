1833cd0c8366aabfaa6980307fcedf7ea97605db
Kayc Mota Farias <kaycg123@gmail.com>
2025-03-12 20:57:02 +0000

    Add Not Found component, robots.txt, and sitemap.xml for improved SEO and user experience

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+
 export default function NotFound() {
     return (
         <div className="flex items-center justify-center min-h-screen light-background dark:dark-background">
