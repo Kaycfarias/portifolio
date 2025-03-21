8b34bedf9d39e63cb99d0e387644147fd89daf09
Kayc Mota Farias <kaycg123@gmail.com>
2025-03-21 22:34:55 +0000

    Remove unused Script import from layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
-import Script from "next/script";
+
 
 const inter = Geist({ subsets: ["latin"] });
 
